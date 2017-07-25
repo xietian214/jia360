0bbf632cceddaa674ded9e0e8ad3a62ec239e91f
zhangmengmeng <zhangmengmeng@yoju360.com>
2017-07-25 11:39:25 +0800

    设计师修改

--- js/designerHome.js
+++ js/designerHome.js
@@ -55,10 +55,55 @@ $(function(){
         className: 'playbox',
         hovbtn:true //悬浮展示左右按钮
     });
+    //设计师列表-查看更多设计师
+    $('.moredesigner').on('click',function(){
+        alert("123");
+        function ajax(page){
+            var _url = window.location.href;
+            _url = _url.replace(/x\/index/,'x\/ajaxlist');
+            ajaxLock = true;
+            $.ajax({
+                url:'data.json',
+                type:"get",
+                dataType:"json",
+                data:{
+                    p : page
+                },
+                success:function(res){
+                    if(res){
+                        var dataList = res.list;
+                        if(dataList.length != 0){
+                            $.each(dataList, function( index, value ){
+                                var _url = 'http://m.jia360.com/cases/index/detail/id/'+value.id+'.html';
+                                var html = '';
+                                html += '<div class="case_box_list"><a href="';
+                                html += _url
+                                html += '"><p class="case_box_list_img"><img src="';
+                                html += value.pic_url
+                                html += '" /></p><p class="case_box_list_text">';
+                                html += value.title
+                                html += '</p></a></div>';
+                                $(html).appendTo($('#waterfall'));
+                            });
+                            $('#waterfall').waterfall();
+                            ajaxLock = false;
+                        }else{
+                            //加载到底
+                            ajaxLock = true;
+                            $('#prompt').css('display','block');
+                        }
+                    }
+                },
+                error: function(e){
+                    console.warn("ajax error")
+                }
+            })
+        }
+    })
     
 })
 //设计师报名
-    var designerSignUp = function(){
+    /*var designerSignUp = function(){
         $('.yysjbtn').on('click',function(){
             $('.designer_tck').css('display','block').children('#designerId').val($(this).attr('designerId'));
             console.log($('#designerId').val());
@@ -131,4 +176,142 @@ $(function(){
         $('.designer_tckinfo .okbtn,.designer_tckinfo .close_btn').on('click',function(){
             $('.designer_tckinfo').css('display','none');
         })
+    }*/
+
+/*设计师详情页案例调用*/
+var _httpAjax = function(){
+    /*瀑布流*/
+    var ajaxLock = false;
+    var page = 2; //page
+    //初始化页面    
+    window.onscroll=function(){
+        var flag = checkscrollside();
+        if(flag && !ajaxLock){
+            ajax(page);
+            page++;
+        };
+    };
+    function checkscrollside(){
+        var $aPin = $( "#waterfall>div" );
+        var lastPinH = 0;
+        //最后一个块框的距离网页顶部+自身高的一半(实现未滚到底就开始加载)
+        $aPin.length == 0 ? lastPinH = 0 : lastPinH = parseInt($aPin.last().offset().top) + Math.floor($aPin.last().height()/2);
+        var scrollTop = $(window).scrollTop()
+        var winH = $(window).height();
+        return (lastPinH < scrollTop + winH ) ? true : false;//到达指定高度后 返回true，触发waterfall()函数
     }
+
+    function ajax(page){
+        var _url = window.location.href;
+        _url = _url.replace(/x\/index/,'x\/ajaxlist');
+        ajaxLock = true;
+        $.ajax({
+            url:'data.json',
+            type:"get",
+            dataType:"json",
+            data:{
+                p : page
+            },
+            success:function(res){
+                if(res){
+                    var dataList = res.list;
+                    if(dataList.length != 0){
+                        $.each(dataList, function( index, value ){
+                            var _url = 'http://m.jia360.com/cases/index/detail/id/'+value.id+'.html';
+                            var html = '';
+                            html += '<div class="case_box_list"><a href="';
+                            html += _url
+                            html += '"><p class="case_box_list_img"><img src="';
+                            html += value.pic_url
+                            html += '" /></p><p class="case_box_list_text">';
+                            html += value.title
+                            html += '</p></a></div>';
+                            $(html).appendTo($('#waterfall'));
+                        });
+                        $('#waterfall').waterfall();
+                        ajaxLock = false;
+                    }else{
+                        //加载到底
+                        ajaxLock = true;
+                        $('#prompt').css('display','block');
+                    }
+                }
+            },
+            error: function(e){
+                console.warn("ajax error")
+            }
+        })
+    }
+}
+
+//设计师设计栏目加载
+var designercolumnLoad = function(){
+    /*瀑布流*/
+    var ajaxLock = false;
+    var page = 2; //page
+    //初始化页面    
+    window.onscroll=function(){
+        var flag = checkscrollside();
+        if(flag && !ajaxLock){
+            ajax(page);
+            page++;
+        };
+    };
+    function checkscrollside(){
+        return ($(document).scrollTop() >= $(document).height() - $(window).height()-250) ? true : false;//到达指定高度后 返回true，触发waterfall()函数
+    }
+
+    function ajax(page){
+        var _url = window.location.href;
+        _url = _url.replace(/x\/index/,'x\/ajaxlist');
+        ajaxLock = true;
+        $.ajax({
+            url:'data1.json',
+            type:"get",
+            dataType:"json",
+            data:{
+                p : page,
+                id : $('.columnListbox ul.active').attr('id')
+            },
+            success:function(res){
+                if(res){
+                    var dataList = res.list;
+                    if(dataList.length != 0){
+                        $.each(dataList, function( index, value ){
+                            var _url = 'http://m.jia360.com/cases/index/detail/id/'+value.id+'.html';
+                            var html = '';
+                            html += '<li><a href="';
+                            html += _url;
+                            html += '" title="'
+                            html += value.title;
+                            html += '" target="_blank" class="imgbox"><img src="';
+                            html += value.pic_url;
+                            html += '" alt="';
+                            html += value.title;
+                            html += '" /></a><a href="';
+                            html += value.title;
+                            html += '" title="';
+                            html += value.title;
+                            html += '" target="_blank" class="fontbox">';
+                            html += value.title;
+                            html += '</a></li>';
+                            //$('.columnListbox ul.active').append(html);
+                            $(html).appendTo($('.columnListbox ul.active'));
+                        });
+                        //$('#waterfall').waterfall();
+                        ajaxLock = false;
+                    }else{
+                        //加载到底
+                        ajaxLock = true;
+                        $('#prompt').css('display','block');
+                    }
+                }
+            },
+            error: function(e){
+                console.warn("ajax error")
+            }
+        })
+    }
+}
+
+
