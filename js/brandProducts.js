d0bc37b498e7dcec36ef409a5e3e0c9efbfe8180
zhangmengmeng <zhangmengmeng@yoju360.com>
2017-09-05 17:31:55 +0800

    产品列表改版

--- js/brandProducts.js
+++ js/brandProducts.js
@@ -21,12 +21,12 @@ $(function(){
     });
 
     //品牌列表鼠标悬浮
-    $('.produL_content,.tuijian').on('mouseenter','li',function(){
+    /*$('.produL_content,.tuijian').on('mouseenter','li',function(){
         $(this).addClass('hov');
     });
     $('.produL_content,.tuijian').on('mouseleave','li',function(){
         $(this).removeClass('hov');
-    });
+    });*/
 
     //产品详情
     var sHeight = $('.proDet_bot .right').outerHeight()-$('.proDet_bot .tuijian').outerHeight()-155;
@@ -84,7 +84,7 @@ $(function(){
         })
     }else{
         $('#braD_moreproduct').css('display','none');
-    };
+    }
     //品牌详情-更多品牌描述
     (function(){
         if($('#brandDet_fontmore').prev('div.fontdiv').height()<75){
@@ -102,10 +102,9 @@ $(function(){
                 $(this).html('展开更多<i>&gt;&gt;</i>').prev('div.fontdiv').height('75px');
                 _flag = 0;
             }
-            
+             
         })
     })();
-    
     //品牌详情-公司店面
     var braD_shoplist = $('.brandDetshop_list li').length;
     if(braD_shoplist%2 == 1){
@@ -135,21 +134,172 @@ $(function(){
         $(window).scrollTop(_scroltop);
         $('.proDet_tabUl li').eq(1).trigger('click');
     })
+
+    //商铺图片切换
+    $('.produL_sort>div').on('click',function(){
+        var sortId = 0;
+        var showId = 0;
+        if($(this).hasClass('sbtn')){
+            $(this).addClass('active').siblings('sbtn').removeClass('active');
+            sortId = $(this).attr('data-sbtn');
+        }else{
+            $(this).addClass('active').siblings('rbtn').removeClass('active');
+            showId = $(this).attr('data-rbtn');
+        };
+        
+        var page = 1;
+        function getmore(){
+            var _url = '/?g=Product&m=Ajax&a=getPcProductList';
+            var brand_id = 0;
+            var category_id = 0;
+            var filter_id = 0;
+            $.ajax({
+                url:_url,
+                type:"get",
+                dataType:"json",
+                data:{
+                    page : page,
+                    brand_id : brand_id,
+                    category_id :category_id,
+                    filter_id : filter_id,
+                    sortId : sortId,
+                    showId : showId
+                },
+                success:function(res){
+                    if(res.length != 0){
+                        $.each(res, function( index, value ){
+                            console.log(value.logo);
+                            var _url = '/product/'+value.id+'.html';
+                            var html = '';
+                            html += '<li><a href="';
+                            html += _url
+                            html += '" target="_blank"  class="a1"><img src="';
+                            html += value.logo
+                            html += '" /></a><a href="';
+                            html += _url
+                            html += '" target="_blank"  class="a2">';
+                            html += value.name
+                            html += '</a><span>参考价格:';
+                            html +=value.price
+                            html +='</span></li>';
+                            $(html).appendTo($('#products'));
+                        });
+                        /*$('#waterfall').waterfall();*/
+                        /*ajaxLock = false;*/
+                    }else{
+                        //加载到底
+                        /*ajaxLock = true;*/
+                        $('#prompt').css('display','block');
+                        $('.productgetmore').css('display','none');
+                    }
+                },
+                error: function(e){
+                    console.warn("ajax error")
+                }
+            })
+            page ++;
+        };
+        shopSeach();
+
+        
+
+
+
+    });
 })
 
 
 //品牌详情-店面搜索
 var shopSeach = function(){
-    var nowProvince = "北京";
-    var nowCity = "东城区";
-    $(".shop_address").citySelect({
-        url:"js/lib/city.min_lk.js",
-        prov:nowProvince,
-        city:nowCity,
-        required:true,
-        nodata:"none"
-    })
     $('#shopSeach').on('click',function(){
-        //调用ajax还木有写
+        var city_code = $("#shopCity").val();
+        var brand_id = $("#brand_id").val();
+        _url = "/?g=product&m=index&a=getBrandStoreByCity";
+        ajaxLock = true;
+        $.ajax({
+            url:_url,
+            type:"get",
+            dataType:"json",
+            data:{
+                city_code : city_code,
+                brand_id : brand_id
+            },
+            success:function(res){
+                if(res.length != 0){
+                    $('#shop_list').html('');
+                    $.each(res, function( index, value ){
+                        var img ='';
+                        if(value.img == ''){
+                            img = 'http://tencentjiaju.img-cn-beijing.aliyuncs.com/20170809/h160w120-598ad3d63f3b8.jpg';
+                        }else{
+                            img = value.img;
+                        }
+                        var html = '';
+                        html += '<li><a href="javascript:;" title="" class="imgbox"><img alt="';
+                        html +=  value.name               
+                        html += '" src="';
+                        html += img
+                        html += '" /></a><div class="fontbox"><h6><a href="javascript:;" title="';
+                        html += value.name
+                        html += '">';
+                        html += value.name
+                        html += '</a></h6><p class="p1">';
+                        html += value.address
+                        html += '</p><p class="p2">';
+                        html += value.contact_way
+                        html += '</p></div></li>';
+                        $(html).appendTo($('#shop_list'));
+                    });
+                    /*$('#waterfall').waterfall();*/
+                    /*ajaxLock = false;*/
+                }else{
+                    //加载到底
+                    /*ajaxLock = true;*/
+                    $('#prompt').css('display','block');
+                }  
+            },
+            error: function(e){
+                console.warn("ajax error")
+            }
+        })
     })
 }
+
+// 获取二级城市
+function getcitys(brand_id){
+    _url = '/?g=Product&m=Ajax&a=getBrandStoreByProv';
+    var brand_id = brand_id;
+    var ad_code = $("#shopProv option:selected").val();
+    $.ajax({
+        url:_url,
+        type:"get",
+        dataType:"json",
+        data:{
+            brand_id : brand_id,
+            ad_code : ad_code
+        },
+        success:function(res){
+            if(res.length != 0){
+                $('#shopCity').html('');
+                $.each(res, function( index, value ){
+                    var html = '';
+                    html += '<option value="';
+                    html += value.city_code
+                    html += '" >';
+                    html += value.name
+                    html += '</option>';
+                    $(html).appendTo($('#shopCity'));
+                });
+                /*$('#waterfall').waterfall();*/
+                /*ajaxLock = false;*/
+            }else{
+                //加载到底
+                /*ajaxLock = true;*/
+                $('#prompt').css('display','block');
+            }  
+        },
+        error: function(e){
+            console.warn("ajax error")
+        }
+    })
+} 
